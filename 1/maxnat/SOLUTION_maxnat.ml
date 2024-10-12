e1ab8caa0159ff2503f8d23c5f6c93d3d7138c8f
HsDL-Galand <matteo.p777@gmail.com>
2024-10-12 16:39:48 +0200

    solution + asserts

---
+++ 1/maxnat/SOLUTION_maxnat.ml
@@ -0,0 +1,39 @@
+(*
+# Max of two natural numbers
+
+Write a function with the following type:
+max_nat : int -> int -> int
+
+such that ``max_nat a b`` evaluates to the maximum between ``a`` and ``b``,
+if they are both *natural* numbers (i.e., greater than or equal to 0).
+If not, the function must fail.
+
+To make a function fail, use the `failwith` construct. For example:
+let always_fail x = failwith("This function always fails")
+
+The following tests must pass:
+assert(max_nat 2 5 = 5);;
+assert(max_nat 5 2 = 5);;
+assert(try max_nat (-2) 5 |> fun _ -> false with _ -> true);;
+assert(try max_nat 2 (-5) |> fun _ -> false with _ -> true);;
+assert(try max_nat (-2) (-5) |> fun _ -> false with _ -> true);;
+
+**Warning**: do not even think of using the built-in ``max`` function.
+*)
+
+let max_nat a b =
+	(*if not(a>=0 && b>=0) then*)
+	if a<0 || b<0 then
+		failwith("Both inputs must be natural numbers")
+	else
+		if a > b then
+			a
+		else
+			b
+
+;;
+assert(max_nat 2 5 = 5);;
+assert(max_nat 5 2 = 5);;
+assert(try max_nat (-2) 5 |> fun _ -> false with _ -> true);;
+assert(try max_nat 2 (-5) |> fun _ -> false with _ -> true);;
+assert(try max_nat (-2) (-5) |> fun _ -> false with _ -> true);;
