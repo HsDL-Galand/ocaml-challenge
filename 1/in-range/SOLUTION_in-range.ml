6e4090e35eae389ce4df2f861efc1805709d3328
HsDL-Galand <matteo.p777@gmail.com>
2024-10-21 17:41:59 +0200

    solution + asserts

---
+++ 1/in-range/SOLUTION_in-range.ml
@@ -0,0 +1,24 @@
+(*
+Write a function with type:
+
+in_range : 'a -> 'a -> 'a -> bool
+such that in_range x a b evaluates to true iff the value x belongs to the range a..b.
+*)
+
+(* in_range : 'a -> 'a -> 'a -> bool *)
+(* let in_range x a b : bool = true *)
+let in_range x a b =
+	if x >= a && x <= b then
+		true
+	else
+		false
+
+;;
+assert(in_range 1 1 1 = true);;
+assert(in_range 2 2 2 = true);;
+assert(in_range 1 2 2 = false);;
+assert(in_range 1 1 2 = true);;
+assert(in_range 1 0 2 = true);;
+assert(in_range 0 (-1) 1 = true);;
+assert(in_range (-3) (-4) (-2) = true);;
+assert(in_range (-6) (-4) (-2) = false);;
