4246d8ba7d2a28dbd786330424ce37dffa5d3ad1
HsDL-Galand <matteo.p777@gmail.com>
2024-10-23 17:00:53 +0200

    solution + asserts

---
+++ 1/7-11-doubles/SOLUTION_7-11-doubles.ml
@@ -0,0 +1,22 @@
+(*
+7, 11 and doubles
+Write a function with type:
+
+seven_eleven_doubles : unit -> bool * int * int
+which throws two random dices d1,d2, and returns a triple (b,d1,d2) where b is true iff the sum of the two dices is 7 or 11, or if the two dices are equal.
+*)
+
+(* seven_eleven_doubles : unit -> bool * int * int *)
+(* let seven_eleven_doubles() : bool * int * int = true, 1, 1 *)
+
+let ad6 = 1+Random.int(6-1+1)
+let bd6 = 1+Random.int(6-1+1)
+let seven_eleven_doubles() =
+	if ad6+bd6=7 || ad6+bd6=11 || ad6=bd6 then
+		true, ad6, bd6
+	else
+		false, ad6, bd6
+
+;;
+(* NON HA SENSO FARE GLI ASSERT CON LA RANDOM *)
+(* assert(seven_eleven_doubles() = (false, ad6, bd6)) *)
