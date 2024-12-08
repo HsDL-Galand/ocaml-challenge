8855ba813526c58d9c222452fa16537875188f08
HsDL-Galand <matteo.p777@gmail.com>
2024-12-08 23:29:15 +0100

    second try

--- 2/both-true/SOLUTION_both-true.ml
+++ 2/both-true/SOLUTION_both-true.ml
@@ -26,3 +26,29 @@ let is_even x = x mod 2 = 0;;
 let is_positive_and_even = both_true is_positive is_even;;
 assert(is_positive_and_even 4 = true);;
 assert(is_positive_and_even 3 = false);;
+
+(*
+Both true
+Write a function both_true that takes as input two predicates p and q, each of type 'a -> bool, and returns a new function that checks if both p and q are true for a given argument.
+
+The function must have the following type:
+
+both_true : ('a -> bool) -> ('a -> bool) -> 'a -> bool
+Here are some unit tests:
+
+let is_positive x = x > 0;;
+let is_even x = x mod 2 = 0;;
+let is_positive_and_even = both_true is_positive is_even;;
+assert(is_positive_and_even 4 = true);;
+assert(is_positive_and_even 3 = false);;
+*)
+
+(* both_true : ('a -> bool) -> ('a -> bool) -> 'a -> bool *)
+let both_true_10 p q v = if(p v)&&(q v) then true else false
+let both_true_11 p q = fun x -> (p x) && (q x)
+
+let is_positive_10 x = x > 0;;
+let is_even_10 x = x mod 2 = 0;;
+let is_positive_and_even_10 = both_true_10 is_positive_10 is_even_10;;
+assert(is_positive_and_even_10 4 = true);;
+assert(is_positive_and_even_10 3 = false);;
