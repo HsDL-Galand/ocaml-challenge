f3bcecba15358ab796e77081e78f8a4b87aaa615
HsDL-Galand <matteo.p777@gmail.com>
2024-12-08 23:18:21 +0100

    second try

--- 2/flip-curry/SOLUTION_flip-curry.ml
+++ 2/flip-curry/SOLUTION_flip-curry.ml
@@ -22,3 +22,28 @@ let sub x y = x - y;;
 let flipped_sub = flip sub;;
 assert (flipped_sub 3 10 = 7);;
 assert (flipped_sub 10 3 = -7);;
+
+
+
+(*
+Flip arguments in curried functions
+Write a function flip that takes as input a function f in curried form and returns a new function that works as f but takes its arguments in the opposite order.
+
+The function must have the following type:
+
+flip : ('a -> 'b -> 'c) -> 'b -> 'a -> 'c
+Here are some unit tests:
+
+let sub x y = x - y;;
+let flipped_sub = flip sub;;
+assert (flipped_sub 3 10 = 7);;
+assert (flipped_sub 10 3 = -7);;
+*)
+
+(* flip_10 : ('a -> 'b -> 'c) -> 'b -> 'a -> 'c *)
+let flip_10 f a b = f b a
+
+let sub_10 x y = x - y;;
+let flipped_sub_10 = flip_10 sub_10;;
+assert (flipped_sub_10 3 10 = 7);;
+assert (flipped_sub_10 10 3 = -7);;
