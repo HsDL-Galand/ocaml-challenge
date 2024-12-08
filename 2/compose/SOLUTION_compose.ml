6ab6ba0cf240c68357af59f6ac394a96d1c5dc64
HsDL-Galand <matteo.p777@gmail.com>
2024-12-08 19:47:04 +0100

    second try

--- 2/compose/SOLUTION_compose.ml
+++ 2/compose/SOLUTION_compose.ml
@@ -41,3 +41,32 @@ let comp_2 f g = fun x -> f(g x)
 (* (a > b)>(Z > a) > Y > b *)
 	let comp_9 f g = fun y -> f(g z)
 *)
+
+(*
+Function composition
+Write a function comp that takes as input two functions f and g and returns a new function that is the composition of f and g (i.e., f (g x)).
+
+The function must have the following type:
+
+comp : ('b -> 'c) -> ('a -> 'b) -> 'a -> 'c
+Here are some unit tests:
+
+let double x = x * 2;;
+let square x = x * x;;
+
+assert((comp square double) 3 = 36);;
+assert((comp double square) 3 = 18);;
+assert((comp String.uppercase_ascii String.trim) "hello" = "HELLO");;
+*)
+
+(* comp : ('b -> 'c) -> ('a -> 'b) -> 'a -> 'c *)
+(* comp : ('a -> 'b) -> ('c -> 'a) -> 'c -> 'b *)
+(* comp : (1b -> 2c) -> (3a -> 1b) -> 3a -> 2c *)
+let comp_10 f g = fun x -> f(g(x))
+
+let double_10 x = x * 2;;
+let square_10 x = x * x;;
+
+assert((comp_10 square_10 double_10) 3 = 36);;
+assert((comp_10 double_10 square_10) 3 = 18);;
+assert((comp_10 String.uppercase_ascii String.trim) "hello" = "HELLO");;
