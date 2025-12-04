b29d81cfd8f85e0fd52427ab6b4293bd806bc892
Galand <matteo.p777@gmail.com>
2025-12-04 15:16:53 +0100

    1-squirrel-play

--- 1/squirrel-play/S_squirrel-play.ml
+++ 1/squirrel-play/S_squirrel-play.ml
@@ -38,8 +38,22 @@ let squirrel_play2 temperature season =
 		true
 	else
 		false
-
 ;;
+
+let squirrel_play3 temperature season = match temperature, season with
+	| _ when (temperature < 15) -> false
+	| _, Summer when (temperature <= 35) -> true
+	| _ when (temperature <= 30) -> true
+	| _ -> false
+;;
+(*
+Summer						40 false
+Summer						32 true
+Spring/Autumn/Winter		32 false
+Spring/Summer/Autumn/Winter	20 true		--
+Spring/Summer/Autumn/Winter	10 false	-
+*)
+
 assert(squirrel_play 18 Winter = true);;
 assert(squirrel_play 32 Spring = false);;
 assert(squirrel_play 32 Summer = true);;
@@ -72,3 +86,38 @@ assert(squirrel_play 33 Spring = false);;
 assert(squirrel_play 33 Summer = true);;
 assert(squirrel_play 33 Autumn = false);;
 assert(squirrel_play 33 Winter = false);;
+
+
+
+assert(squirrel_play3 18 Winter = true);;
+assert(squirrel_play3 32 Spring = false);;
+assert(squirrel_play3 32 Summer = true);;
+
+assert(squirrel_play3 10 Spring = false);;
+assert(squirrel_play3 15 Spring = true);;
+assert(squirrel_play3 20 Spring = true);;
+assert(squirrel_play3 30 Spring = true);;
+assert(squirrel_play3 40 Spring = false);;
+
+assert(squirrel_play3 10 Summer = false);;
+assert(squirrel_play3 15 Summer = true);;
+assert(squirrel_play3 20 Summer = true);;
+assert(squirrel_play3 30 Summer = true);;
+assert(squirrel_play3 40 Summer = false);;
+
+assert(squirrel_play3 10 Autumn = false);;
+assert(squirrel_play3 15 Autumn = true);;
+assert(squirrel_play3 20 Autumn = true);;
+assert(squirrel_play3 30 Autumn = true);;
+assert(squirrel_play3 40 Autumn = false);;
+
+assert(squirrel_play3 10 Winter = false);;
+assert(squirrel_play3 15 Winter = true);;
+assert(squirrel_play3 20 Winter = true);;
+assert(squirrel_play3 30 Winter = true);;
+assert(squirrel_play3 40 Winter = false);;
+
+assert(squirrel_play3 33 Spring = false);;
+assert(squirrel_play3 33 Summer = true);;
+assert(squirrel_play3 33 Autumn = false);;
+assert(squirrel_play3 33 Winter = false);;
