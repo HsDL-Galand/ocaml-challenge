972a4c63fe864b9fac0aa8415aa639f92be916e5
Galand <matteo.p777@gmail.com>
2025-12-04 15:30:14 +0100

    1-exam-committee

--- 1/exam-committee/S_exam-committee.ml
+++ 1/exam-committee/S_exam-committee.ml
@@ -28,6 +28,23 @@ let decide_exam vote1 vote2 vote3 =
 		false
 
 ;;
+
+let decide_exam2 vote1 vote2 vote3 = match vote1, vote2, vote3 with
+	| _ when (vote1 = StrongReject || vote2 = StrongReject || vote3 = StrongReject) -> false
+	| _ when (	((vote1 = StrongAccept || vote1 = WeakAccept)   &&   (vote2 = StrongAccept || vote2 = WeakAccept))	||
+				((vote2 = StrongAccept || vote2 = WeakAccept)   &&   (vote3 = StrongAccept || vote3 = WeakAccept))	||
+				((vote1 = StrongAccept || vote1 = WeakAccept)   &&   (vote3 = StrongAccept || vote3 = WeakAccept))	) -> true
+	| _ -> false
+;;
+
+(*
+_ v1 || v2 || v3 = StrongReject -> false
+v1 = SA || v1 WA   && v2 = SA || v2 WA     ||
+v2 = SA || v2 WA   && v3 = SA || v3 WA     ||
+v1 = SA || v1 WA   && v3 = SA || v3 WA     -> true
+_ -> false
+*)
+
 assert(decide_exam StrongAccept StrongAccept StrongAccept = true);;
 assert(decide_exam WeakAccept WeakAccept WeakAccept = true);;
 assert(decide_exam WeakReject WeakReject WeakReject = false);;
@@ -37,3 +54,14 @@ assert(decide_exam StrongAccept StrongAccept WeakAccept = true);;
 assert(decide_exam StrongAccept StrongAccept WeakReject = true);;
 assert(decide_exam StrongAccept StrongAccept StrongReject = false);;
 assert(decide_exam StrongAccept WeakReject WeakReject = false);;
+
+
+assert(decide_exam2 StrongAccept StrongAccept StrongAccept = true);;
+assert(decide_exam2 WeakAccept WeakAccept WeakAccept = true);;
+assert(decide_exam2 WeakReject WeakReject WeakReject = false);;
+assert(decide_exam2 StrongReject StrongReject StrongReject = false);;
+
+assert(decide_exam2 StrongAccept StrongAccept WeakAccept = true);;
+assert(decide_exam2 StrongAccept StrongAccept WeakReject = true);;
+assert(decide_exam2 StrongAccept StrongAccept StrongReject = false);;
+assert(decide_exam2 StrongAccept WeakReject WeakReject = false);;
