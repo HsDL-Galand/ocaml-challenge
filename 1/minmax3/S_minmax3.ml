5558c499de1435bd2d7583ae0b346da965fdd223
Galand <matteo.p777@gmail.com>
2025-12-04 20:16:52 +0100

    Update S_minmax3.ml

--- 1/minmax3/S_minmax3.ml
+++ 1/minmax3/S_minmax3.ml
@@ -88,6 +88,10 @@ assert(minmax3_2 (-4) (-3) (-2) = ((-4),(-2)));;
 
 
 assert(minmax3_3 1 2 3 = (1,3));;
+assert(minmax3_3 1 3 2 = (1,3));;
+assert(minmax3_3 2 1 3 = (1,3));;
+assert(minmax3_3 2 3 1 = (1,3));;
+assert(minmax3_3 3 1 2 = (1,3));;
 assert(minmax3_3 3 2 1 = (1,3));;
 assert(minmax3_3 10 30 20 = (10,30));;
 assert(minmax3_3 22 11 33 = (11,33));;
