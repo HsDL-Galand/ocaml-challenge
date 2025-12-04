32157da2a5de7e23b1f3c14239072d7a912f5e70
Galand <matteo.p777@gmail.com>
2025-12-04 19:53:00 +0100

    1-head-or-tail

--- 1/head-or-tail/S_head-or-tail.ml
+++ 1/head-or-tail/S_head-or-tail.ml
@@ -13,19 +13,32 @@ to generate a random integer in the range 0..n-1.
 
 (* hot : unit -> string *)
 (* let hot (n : unit) : string = "head" *)
-let hot n =
+let hot (n) =
 	if (Random.int n) mod 2 = 0 then
 		"head"
 	else
 		"tail"
-(* QUESTA è SBAGLIATA *)
+(* hot QUESTA è SBAGLIATA *)
 
 
 let hot_2() =
-	if Random.int 2 = 0 then
+	if (Random.int 2) = 0 then
 		"head"
 	else
 		"tail"
+
+
+let hot_3() = match () with
+	| _ when (Random.int 2 mod 2 = 0) -> "head"
+	| _ -> "tail"
+
+
+let hot_4() = function
+	| _ when (Random.int 2 mod 2 = 0) -> "head"
+	| _ -> "tail"
+(* hot_4 QUESTA è SBAGLIATA, funcion non riesco a farlo funzionare con zero parametri *)
+
+
 ;;
 (* Gli assert non hanno molto senso di esistere, dato che è una generazione randomica*)
 (* ERRORE: Random.int non accetta input < 0 *)
