bc7f5d9ede1d2c31b5d922388763e3ba1337c37b
Galand <matteo.p777@gmail.com>
2025-12-05 20:43:31 +0100

    1-7-11-doubles

--- 1/7-11-doubles/S_7-11-doubles.ml
+++ 1/7-11-doubles/S_7-11-doubles.ml
@@ -11,11 +11,18 @@ which throws two random dices d1,d2, and returns a triple (b,d1,d2) where b is t
 
 let ad6 = 1+Random.int(6-1+1)
 let bd6 = 1+Random.int(6-1+1)
-let seven_eleven_doubles() =
+let seven_eleven_doubles() =	(* SOLUZIONE MOLTO DISCUTIBILE *)
 	if ad6+bd6=7 || ad6+bd6=11 || ad6=bd6 then
 		true, ad6, bd6
 	else
 		false, ad6, bd6
+
+
+let seven_eleven_doubles2() =
+	let a =	Random.int(6)+1 in
+	let b = Random.int(6)+1 in
+		(a+b=7 || a+b=11 || a=b,a,b)
+
 
 ;;
 (* NON HA SENSO FARE GLI ASSERT CON LA RANDOM *)
