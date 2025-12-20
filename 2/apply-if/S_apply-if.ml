225cd5318125ac0f708baa924564df1c1e00d3f4
Galand <matteo.p777@gmail.com>
2025-12-20 13:06:18 +0100

    2-apply-if

--- 2/apply-if/S_apply-if.ml
+++ 2/apply-if/S_apply-if.ml
@@ -36,3 +36,31 @@ assert(apply_if is_even double 4 = 8);;
 assert(apply_if is_even double 5 = 5);;
 assert(apply_if is_odd double 4 = 4);;
 assert(apply_if is_odd double 5 = 10);;
+
+(*********************************************************** \/ SOLUZIONE 2025 \/ ***********************************************************)
+
+let apply_if2 p f x =
+	if (p x)
+		then f x
+		else x
+
+let is_even = fun x -> x mod 2 = 0;;
+let is_odd = fun x -> not (is_even x);;
+let double x = x * 2;;
+
+assert(apply_if2 is_even double 4 = 8);;
+assert(apply_if2 is_even double 5 = 5);;
+assert(apply_if2 is_odd double 4 = 4);;
+assert(apply_if2 is_odd double 5 = 10);;
+
+(********************************************************** \/ GEMINI \/ **********************************************************)
+
+let apply_if p f x = if p x then f x else x
+
+(* Assert Tests *)
+let () =
+  let is_even x = x mod 2 = 0 in
+  let double x = x * 2 in
+  assert (apply_if is_even double 4 = 8);
+  assert (apply_if is_even double 5 = 5);
+  assert (apply_if (fun _ -> false) double 10 = 10)
